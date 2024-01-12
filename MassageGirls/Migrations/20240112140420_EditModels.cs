c48b1eb3ec81654a7b2b1608e47d8edd1d1ca80e
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2024-01-12 17:48:36 +0200

    Migrations

---
+++ MassageGirls/Migrations/20240112140420_EditModels.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MassageGirls.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MassageTypes",
+                columns: table => new
+                {
+                    MassageTypeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MassageTypes", x => x.MassageTypeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GirlProfiles",
+                columns: table => new
+                {
+                    GirlId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GirlName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Photo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Town = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MassageTypeID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GirlProfiles", x => x.GirlId);
+                    table.ForeignKey(
+                        name: "FK_GirlProfiles_MassageTypes_MassageTypeID",
+                        column: x => x.MassageTypeID,
+                        principalTable: "MassageTypes",
+                        principalColumn: "MassageTypeID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GirlProfiles_MassageTypeID",
+                table: "GirlProfiles",
+                column: "MassageTypeID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GirlProfiles");
+
+            migrationBuilder.DropTable(
+                name: "MassageTypes");
+        }
+    }
+}
---
+++ MassageGirls/Migrations/20240112141644_AddModels2.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MassageGirls.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModels2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GirlProfiles_MassageTypes_MassageTypeID",
+                table: "GirlProfiles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MassageTypes",
+                table: "MassageTypes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GirlProfiles",
+                table: "GirlProfiles");
+
+            migrationBuilder.RenameTable(
+                name: "MassageTypes",
+                newName: "MassageType");
+
+            migrationBuilder.RenameTable(
+                name: "GirlProfiles",
+                newName: "GirlProfile");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_GirlProfiles_MassageTypeID",
+                table: "GirlProfile",
+                newName: "IX_GirlProfile_MassageTypeID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MassageType",
+                table: "MassageType",
+                column: "MassageTypeID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GirlProfile",
+                table: "GirlProfile",
+                column: "GirlId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GirlProfile_MassageType_MassageTypeID",
+                table: "GirlProfile",
+                column: "MassageTypeID",
+                principalTable: "MassageType",
+                principalColumn: "MassageTypeID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GirlProfile_MassageType_MassageTypeID",
+                table: "GirlProfile");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MassageType",
+                table: "MassageType");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GirlProfile",
+                table: "GirlProfile");
+
+            migrationBuilder.RenameTable(
+                name: "MassageType",
+                newName: "MassageTypes");
+
+            migrationBuilder.RenameTable(
+                name: "GirlProfile",
+                newName: "GirlProfiles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_GirlProfile_MassageTypeID",
+                table: "GirlProfiles",
+                newName: "IX_GirlProfiles_MassageTypeID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MassageTypes",
+                table: "MassageTypes",
+                column: "MassageTypeID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GirlProfiles",
+                table: "GirlProfiles",
+                column: "GirlId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GirlProfiles_MassageTypes_MassageTypeID",
+                table: "GirlProfiles",
+                column: "MassageTypeID",
+                principalTable: "MassageTypes",
+                principalColumn: "MassageTypeID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
