86881dd45bc23cb39e371753a2f874ada53297f9
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2024-01-12 15:46:30 +0200

    Add connection to DB

---
+++ MassageGirls/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MassageGirls.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MassageGirls.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<GirlProfile> GirlProfiles { get; set; }
+        public DbSet<MassageType> MassageTypes { get; set; }
+    }
+}
--- MassageGirls/Program.cs
+++ MassageGirls/Program.cs
@@ -1,7 +1,14 @@
+using MassageGirls.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("MassageGirlsOnlineConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -12,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
