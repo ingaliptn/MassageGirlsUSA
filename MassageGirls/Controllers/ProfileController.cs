06668eecd8455052ddc571c2ead9853b0cdfcc95
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2024-01-12 17:49:44 +0200

    Create Profile Controller and Add Test Functional for Showing Data

---
+++ MassageGirls/Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using MassageGirls.Context;
+using MassageGirls.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MassageGirls.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly AppDbContext _db;
+        public ProfileController(AppDbContext bd)
+        {
+            _db = bd;
+        }
+        public IActionResult Index(int? id)
+        {
+            GirlProfile Girls;
+            if (id == null)
+                Girls = _db.GirlProfile.FirstOrDefault();
+            else
+                Girls = _db.GirlProfile.Where(x => x.GirlId == id).FirstOrDefault();
+
+            return View(Girls);
+        }
+    }
+}
