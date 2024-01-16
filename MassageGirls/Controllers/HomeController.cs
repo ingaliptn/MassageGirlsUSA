7b6c5ba5ec9f4a0e93773765b5c9913a097595ab
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2024-01-17 00:20:53 +0200

    Create Functional for Correct DataView

--- MassageGirls/Controllers/HomeController.cs
+++ MassageGirls/Controllers/HomeController.cs
@@ -33,15 +33,71 @@ namespace MassageGirls.Controllers
         public IActionResult Cities(int? TownId, int? MassageId)
         {
             var town = _db.Town.FirstOrDefault(t => t.TownID == TownId);
+            var massage = _db.MassageType.FirstOrDefault(y => y.MassageTypeID == MassageId);
+            //List<GirlProfile> townGirls = _db.GirlProfile.Where(x => x.TownID == TownId).Select(a => a.GirlName).ToList();
 
-            // Use town information to customize the view
+
+            //ViewData["Girls"] = townGirls;
             ViewData["TownId"] = town.TownID;
             ViewData["TownName"] = town.TownName;
-            ViewData["Header"] = town.EroticHeader; // Replace with actual property name
-            ViewData["Footer"] = town.EroticFooter;
+            ViewData["MassageType"] = massage.TypeName;
 
-            IEnumerable<GirlProfile> Girls = _db.GirlProfile.Where(girl => girl.Town.TownID == TownId && girl.MassageTypeID == MassageId);
-            return View(Girls);
+            // Determine the appropriate header and footer based on MassageType
+            string header = "";
+            string footer = "";
+
+            switch (MassageId)
+            {
+                case 1: // Assume MassageTypeID 1 corresponds to Erotic Massage
+                    header = town.EroticHeader;
+                    footer = town.EroticFooter;
+                    break;
+
+                case 2: // Assume MassageTypeID 2 corresponds to Asian Massage
+                    header = town.HappyEndingHeader;
+                    footer = town.HappyEndingFooter;
+                    break;
+                case 3:
+                    header = town.TantraHeader;
+                    footer = town.TantraFooter;
+                    break;
+
+                case 4: 
+                    header = town.CouplesHeader;
+                    footer = town.CouplesFooter;
+                    break;
+                case 5: 
+                    header = town.AsianHeader;
+                    footer = town.AsianFooter;
+                    break;
+
+                case 6: 
+                    header = town.NuruHeader;
+                    footer = town.NuruFooter;
+                    break;
+                case 7:
+                    header = town.BodyHeader;
+                    footer = town.BodyFooter;
+                    break;
+
+                case 8: 
+                    header = town.SensualHeader;
+                    footer = town.SensualFooter;
+                    break;
+
+                default:
+                    break;
+            }
+
+            // Pass the header and footer to the view
+            ViewData["Header"] = header;
+            ViewData["Footer"] = footer;
+
+            IEnumerable<GirlProfile> girls = _db.GirlProfile
+                .Where(girl => girl.Town.TownID == TownId && girl.MassageTypeID == MassageId)
+                .ToList();
+
+            return View(girls);
         }
         public IActionResult OurMassageGirls()
         {
