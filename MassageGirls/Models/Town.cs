f0a5c60de47bd84d17db0c61496aba4c34ae75e1
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2024-01-16 16:47:36 +0200

    Add fields in Town Model

--- MassageGirls/Models/Town.cs
+++ MassageGirls/Models/Town.cs
@@ -7,5 +7,21 @@ namespace MassageGirls.Models
         [Key]
         public int TownID { get; set; }
         public string TownName { get; set; }
+        public string EroticHeader { get; set; }
+        public string EroticFooter { get; set; }
+        public string HappyEndingHeader { get; set; }
+        public string HappyEndingFooter { get; set; }
+        public string TantraHeader { get; set; }
+        public string TantraFooter { get; set; }
+        public string CouplesHeader { get; set; }
+        public string CouplesFooter { get; set; }
+        public string AsianHeader { get; set; }
+        public string AsianFooter { get; set; }
+        public string NuruHeader { get; set; }
+        public string NuruFooter { get; set; }
+        public string BodyHeader { get; set; }
+        public string BodyFooter { get; set; }
+        public string SensualHeader { get; set; }
+        public string SensualFooter { get; set; }
     }
 }
