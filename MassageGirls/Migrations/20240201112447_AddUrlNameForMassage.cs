06384298bce71685ffcf9f1e5df338a594eaf8a3
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2024-02-01 14:05:12 +0200

    Add Field to Massage Table

---
+++ MassageGirls/Migrations/20240201112447_AddUrlNameForMassage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MassageGirls.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUrlNameForMassage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UrlName",
+                table: "MassageType",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UrlName",
+                table: "MassageType");
+        }
+    }
+}
